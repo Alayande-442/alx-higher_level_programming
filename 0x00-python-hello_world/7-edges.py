1ee890d6173d3d96044ee195aabc71e742915325
Alayande-442 <alayandesteven@gmail.com>
2024-01-15 13:51:07 -0800

    task-7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_3 = word[2]
+word_2 = word[-2]
+midd = word[1:-1]
+print(f"First 3 letters: {word_3}")
+print(f"Last 2 letters: {word_2}")
+print(f"Middle word: {midd}")
