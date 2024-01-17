a4405bfbdb3c7e7b5ccf2a633d838c7d7e3e5725
Alayande-442 <alayandesteven@gmail.com>
2024-01-16 21:10:20 -0800

    task-5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print("{:02d}".format(i), end=", ")
-print("{:02d}".format(0, 100))
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02d}, ".format(i), end="")
