d55a5388edcd1b4ce64cfca46637aa8b10edb6f9
Alayande-442 <alayandesteven@gmail.com>
2024-02-06 08:20:20 -0800

    task-0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    counts = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            counts += 1
+        except IndexError:
+            break
+    print("")
+    return counts
