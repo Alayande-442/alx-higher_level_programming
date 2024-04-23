68acd8b055956ba8f2af4f67074b2b4b3e237230
Alayande-442 <alayandesteven@gmail.com>
2024-04-22 19:40:53 -0700

    task-0

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let firstName = "C is fun";
+let secondName = "Python is cool";
+let thirdName = "JavaScript is amazing";
+console.log(firstName);
+console.log(secondName);
+console.log(thirdName);
