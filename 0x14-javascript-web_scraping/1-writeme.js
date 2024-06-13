3153ff9211bf154b9044c5df7614d17721518667
Alayande-442 <alayandesteven@gmail.com>
2024-06-13 20:22:43 +0000

    task-1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,22 +1,9 @@
 #!/usr/bin/node
 
 const fs = require("fs");
+const file = process.argv[2];
+const string = process.argv[3];
 
-const filePath = process.argv[2];
-
-const stringToWrite = process.argv[3];
-
-if (!filePath || !stringToWrite) {
-  console.error(
-    "Please provide both a file path and a string to write as arguments."
-  );
-  process.exit(1);
-}
-
-fs.writeFile(filePath, stringToWrite, "utf-8", (err) => {
-  if (err) {
-    console.error("Error writing to the file:", err);
-  } else {
-    console.log("String written to file successfully.");
-  }
+fs.writeFile(file, string, "utf-8", function (err) {
+  if (err) console.log(err);
 });
